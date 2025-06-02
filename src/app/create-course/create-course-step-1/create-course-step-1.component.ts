f56e46f716d73f5678fa961a10e32ad4ec572768
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-06-02 22:28:12 +0330

    Angular Material Date Picker - Highlighting a Calendar Date

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,5 +1,9 @@
 import { Component } from "@angular/core";
 import { UntypedFormBuilder, Validators } from "@angular/forms";
+import {
+  MatCalendarCell,
+  MatCalendarCellClassFunction,
+} from "@angular/material/datepicker";
 
 @Component({
   selector: "create-course-step-1",
@@ -19,6 +23,16 @@ export class CreateCourseStep1Component {
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: ["", [Validators.required, Validators.minLength(3)]],
   });
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    const date = cellDate.getDate();
+    if (view === "month") {
+      // We can pass multiple css classes seperated by space
+      return date === 1 ? "highlight-date second-css-class" : "";
+    }
+
+    return "";
+  };
 
   constructor(private fb: UntypedFormBuilder) {}
 
